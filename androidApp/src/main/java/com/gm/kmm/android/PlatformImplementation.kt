54762098a5366913383fafb1535d911668e456b3
Georgios Metaxakis <g.metaxakis@withplum.com>
2021-04-10 23:45:38 +0300

    remove usage of platform class, keep it for tests

--- androidApp/src/main/java/com/gm/kmm/android/PlatformImplementation.kt
+++ androidApp/src/main/java/com/gm/kmm/android/PlatformImplementation.kt
@@ -1,10 +1,9 @@
 package com.gm.kmm.android
 
-import com.gm.kmm.Platform
 import com.gm.kmm.PlatformRepo
 
 class PlatformImplementation : PlatformRepo {
     override fun getPlatform(): String {
-        return Platform().platform
+        return "Android ${android.os.Build.VERSION.SDK_INT}"
     }
 }
